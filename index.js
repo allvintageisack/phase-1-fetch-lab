a8d98de9c4aed8c56090eaf5ceb44f4a378f96f4
isackmwangasu <mwangasuisack@gmail.com>
2024-04-14 11:14:56 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch books');
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+    });
 }
+
+document.addEventListener('DOMContentLoaded', fetchBooks);
+
+
+
+// Call fetchBooks() when the index.html is loaded
+document.addEventListener('DOMContentLoaded', fetchBooks);
 
 function renderBooks(books) {
   const main = document.querySelector('main');
